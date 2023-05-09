cd4a9d31b858467f256909cb176b9094149252d2
aniketkumar01 <aniket.kumar01@nagarro.com>
2023-05-10 04:07:27 +0530

    Added Sonar scanner in Frontend in pom.xml

--- Week9/AdvanceJavaAssignment5Frontend/src/main/java/com/nagarro/initializer/AppIntializer.java
+++ Week9/AdvanceJavaAssignment5Frontend/src/main/java/com/nagarro/initializer/AppIntializer.java
@@ -2,8 +2,6 @@ package com.nagarro.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AppIntializer {
@@ -11,5 +9,5 @@ public class AppIntializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AppIntializer.class, args);
 	}
-	
+
 }
