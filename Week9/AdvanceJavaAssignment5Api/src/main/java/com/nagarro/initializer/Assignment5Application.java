d5c81b642bf0bf68716bde2bd46a2ec4ec22c15a
aniketkumar01 <aniket.kumar01@nagarro.com>
2023-05-10 03:54:16 +0530

    Added Sonar Scanner Dependency in Pom.xml

--- Week9/AdvanceJavaAssignment5Api/src/main/java/com/nagarro/initializer/Assignment5Application.java
+++ Week9/AdvanceJavaAssignment5Api/src/main/java/com/nagarro/initializer/Assignment5Application.java
@@ -1,8 +1,11 @@
 package com.nagarro.initializer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class Assignment5Application {
 
 	public static void main(String[] args) {
